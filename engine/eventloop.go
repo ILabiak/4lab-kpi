d4ca6ecc2a10b5e1b480b259d0aac174e4c88cde
akaeyuhi <54174780+akaeyuhi@users.noreply.github.com>
2022-06-13 21:18:47 +0300

    EventLoop implementation

---
+++ engine/eventloop.go
@@ -0,0 +1,32 @@
+package engine
+
+type EventLoop struct {
+	q *Queue
+
+	stop       bool
+	stopSignal chan struct{}
+}
+
+func (l *EventLoop) Start() {
+	l.q = &Queue{
+		notEmptySignal: make(chan struct{}),
+	}
+	l.stopSignal = make(chan struct{})
+	go func() {
+		for !l.stop || !l.q.empty() {
+			cmd := l.q.pull()
+			cmd.Execute(l)
+		}
+		l.stopSignal <- struct{}{}
+	}()
+}
+
+func (l *EventLoop) Post(cmd Command) {
+	l.q.push(cmd)
+}
+
+func (l *EventLoop) AwaitFinish() {
+	l.Post(stopCommand{})
+	l.stop = true
+	<-l.stopSignal
+}
